6d063840c4e1ec6b742f65a1fc94da0be16a4d33
Artem Labazin <xxlabaza@gmail.com>
2016-03-25 12:52:23 +0300

    Initial commit

---
+++ filters/MyFilter.groovy
@@ -0,0 +1,39 @@
+package ru.xxlabaza.test.zuul.filters.groovy
+
+import javax.servlet.http.HttpServletRequest
+import com.netflix.zuul.context.RequestContext
+import com.netflix.zuul.ZuulFilter
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class)
+
+    boolean shouldFilter () {
+        return true
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext()
+        HttpServletRequest request = ctx.getRequest()
+
+        log.info(String.format('\n\n%s request to %s\n', request.method, request.requestURL.toString()))
+
+        return Boolean.TRUE
+    }
+
+    int filterOrder () {
+        return 1
+    }
+
+    String filterType () {
+        return 'pre'
+    }
+
+    String disablePropertyName () {
+        return 'zuul.MyFilter.disable'
+    }
+}
